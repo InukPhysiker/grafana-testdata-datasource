12c84616c86ddb0a14ec7af9dffd44000d901499
InukPhysiker <InukPhysiker@gmail.com>
2020-02-20 19:58:54 -0600

    Use DataSourceHttpSettings

--- src/app/plugins/datasource/simple-epics/components/ConfigEditor.tsx
+++ src/app/plugins/datasource/simple-epics/components/ConfigEditor.tsx
@@ -1,81 +1,18 @@
-import React, { PureComponent, ChangeEvent } from 'react';
-import { SecretFormField, FormField } from '@grafana/ui';
+import React from 'react';
+import { DataSourceHttpSettings } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { EpicsDataSourceOptions, EpicsSecureJsonData } from '../types';
+import { EpicsDataSourceOptions } from '../types';
 
-interface Props extends DataSourcePluginOptionsEditorProps<EpicsDataSourceOptions> {}
+export const ConfigEditor = (props: DataSourcePluginOptionsEditorProps<EpicsDataSourceOptions>) => {
+  const { options, onOptionsChange } = props;
 
-interface State {}
-
-export class ConfigEditor extends PureComponent<Props, State> {
-  onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    const jsonData = {
-      ...options.jsonData,
-      path: event.target.value,
-    };
-    onOptionsChange({ ...options, jsonData });
-  };
-
-  // Secure field (only sent to th ebackend)
-  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonData: {
-        apiKey: event.target.value,
-      },
-    });
-  };
-
-  onResetAPIKey = () => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonFields: {
-        ...options.secureJsonFields,
-        apiKey: false,
-      },
-      secureJsonData: {
-        ...options.secureJsonData,
-        apiKey: '',
-      },
-    });
-  };
-
-  render() {
-    const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
-    const secureJsonData = (options.secureJsonData || {}) as EpicsSecureJsonData;
-
-    return (
-      <div className="gf-form-group">
-        <div className="gf-form">
-          <FormField
-            label="Path"
-            labelWidth={6}
-            inputWidth={20}
-            onChange={this.onPathChange}
-            value={jsonData.path || ''}
-            placeholder="json field returned to frontend"
-          />
-        </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
-              placeholder="secure json field (backend only)"
-              labelWidth={6}
-              inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <>
+      <DataSourceHttpSettings
+        defaultUrl="http://archiver.clsi.ca:17668"
+        dataSourceConfig={options}
+        onChange={onOptionsChange as any}
+      />
+    </>
+  );
+};
