567378aea420a1e46594954e42f55ef1f47c6e3a
InukPhysiker <InukPhysiker@gmail.com>
2020-02-24 17:33:15 -0600

    Update bin size handling

--- src/app/plugins/datasource/cloudwatch-based/url_builder.ts
+++ src/app/plugins/datasource/cloudwatch-based/url_builder.ts
@@ -1,51 +1,13 @@
-import moment from 'moment';
+// import moment from 'moment';
 
 export default class UrlBuilder {
   static buildArchiveRetrievalUrl(pvname: string, operator: string, range: any, intervalMs: number, maxDataPoints: number) {
     // TODO: deal with maxDataPoints
 
-    // let startTime = new Date(range.from);
-    // let stopTime = new Date(range.to);
-
-    // ISO string required by EPICS Archiver Appliance
-    // let startTimeISO = startTime.toISOString();
-    // let stopTimeISO = stopTime.toISOString();
-
-    // milliseconds required for time range calculation
-    // let startTime_ms = startTime.getTime();
-    // let stopTime_ms = stopTime.getTime();
-
-    // let time_range = moment.duration(stopTime_ms - startTime_ms);
-    const timeRange = moment.duration(intervalMs);
+    // const timeRange = moment.duration(intervalMs);
 
     // EPICS Archiver Appliances uses the following sampling values:
-    //
-    // 30m --> lastSample_5
-    //  4h --> lastSample_15
-    //  8h --> lastSample_30
-    //  1d --> lastSample_120
-    //  2d --> lastSample_180
-    //  1w --> lastSample_600
-    //  2w --> lastSample_1200
-    //  1M --> lastSample_3600
-    //  6M --> lastSample_14400
-    //  1Y --> lastSample_43200
-
-    // setSamplingOption does the same and interpolates for other time ranges
-
-    let sampleRate = 1; // retrieve all data points
-
-    if (timeRange.asDays() < 1) {
-      sampleRate = 5 * Math.round((3.5 * timeRange.asHours() + 1) / 5.0) || 1;
-    } else if (timeRange.asWeeks() < 1) {
-      sampleRate = 60 * Math.round(timeRange.asDays());
-    } else if (timeRange.asMonths() < 1) {
-      sampleRate = 600 * Math.round(timeRange.asWeeks());
-    } else if (Math.round(timeRange.asMonths()) === 6) {
-      sampleRate = 14440;
-    } else if (timeRange.asMonths() >= 1) {
-      sampleRate = 3600 * Math.round(timeRange.asMonths());
-    }
+    const binSize = determineBinSize(intervalMs, maxDataPoints);
 
     let retrievalParameters = '';
 
@@ -78,8 +40,27 @@ export default class UrlBuilder {
     }
 
     retrievalParameters +=
-      sampleRate + '(' + pvname + ')' + '&from=' + range.from.toISOString() + '&to=' + range.to.toISOString() + '&fetchLatestMetadata=false';
+      binSize + '(' + pvname + ')' + '&from=' + range.from.toISOString() + '&to=' + range.to.toISOString() + '&fetchLatestMetadata=false';
 
     return retrievalParameters;
   }
+}
+
+// Compute the bin size for the operators automatically based on the number of points in the plot window and (endTime - startTime).
+function determineBinSize(intervalMs: number, maxDataPoints: number) {
+	var duration = intervalMs/1000;
+  var points = maxDataPoints;
+  var binSize: number;
+	if(duration <= 2*points) {
+		binSize = 0;
+	}
+	var potentialBinSizes = [5, 10, 15, 30, 60, 120, 180, 300, 600, 1200, 1800, 3600, 7200, 14400, 21600, 43200, 86400];
+	for(const i in potentialBinSizes) {
+		var potentialBinSize = potentialBinSizes[i];
+		if((duration/potentialBinSize) <= 2*maxDataPoints) {
+			binSize = potentialBinSize;
+			break;
+		}
+  }
+  return binSize;
 }
