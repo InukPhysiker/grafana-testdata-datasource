ccd2033dfef3dfa4922f6d363fa1b1600229cba2
InukPhysiker <InukPhysiker@gmail.com>
2020-02-27 19:45:42 -0600

    Add imports

--- src/app/plugins/datasource/cloudwatch-based/module.ts
+++ src/app/plugins/datasource/cloudwatch-based/module.ts
@@ -3,6 +3,8 @@ import EpicsDataSource from './datasource';
 import { ConfigEditor } from './components/ConfigEditor';
 import { QueryEditor } from './components/QueryEditor';
 import { EpicsQuery, EpicsJsonData } from './types';
+import './add-metric-function.directive';
+import './metric-function-editor.directive';
 
 export const plugin = new DataSourcePlugin<EpicsDataSource, EpicsQuery, EpicsJsonData>(EpicsDataSource)
   .setConfigEditor(ConfigEditor)
