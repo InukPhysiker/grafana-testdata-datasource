359fd6e355a7eedcf339c66d07e2ff69a5e892a3
InukPhysiker <InukPhysiker@gmail.com>
2020-02-29 15:26:21 -0600

    Import query_parameter_ctrl

--- src/app/plugins/datasource/cloudwatch-based/module.ts
+++ src/app/plugins/datasource/cloudwatch-based/module.ts
@@ -5,6 +5,8 @@ import { QueryEditor } from './components/QueryEditor';
 import { EpicsQuery, EpicsJsonData } from './types';
 import './add-metric-function.directive';
 import './metric-function-editor.directive';
+import './query_parameter_ctrl';
+
 
 export const plugin = new DataSourcePlugin<EpicsDataSource, EpicsQuery, EpicsJsonData>(EpicsDataSource)
   .setConfigEditor(ConfigEditor)
