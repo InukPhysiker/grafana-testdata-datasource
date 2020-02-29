018d1b56b495903bcd15cfa00c3c2cb6ce418391
InukPhysiker <InukPhysiker@gmail.com>
2020-02-29 09:42:36 -0600

    clean-up comments

--- src/app/plugins/datasource/cloudwatch-based/module.ts
+++ src/app/plugins/datasource/cloudwatch-based/module.ts
@@ -9,22 +9,3 @@ import './metric-function-editor.directive';
 export const plugin = new DataSourcePlugin<EpicsDataSource, EpicsQuery, EpicsJsonData>(EpicsDataSource)
   .setConfigEditor(ConfigEditor)
   .setQueryEditor(QueryEditor);
-
-// CloudWatch
-
-// import './query_parameter_ctrl';
-
-// import CloudWatchDatasource from './datasource';
-// import { CloudWatchQueryCtrl } from './query_ctrl';
-// import { CloudWatchConfigCtrl } from './config_ctrl';
-
-// class CloudWatchAnnotationsQueryCtrl {
-//   static templateUrl = 'partials/annotations.editor.html';
-// }
-
-// export {
-//   CloudWatchDatasource as Datasource,
-//   CloudWatchQueryCtrl as QueryCtrl,
-//   CloudWatchConfigCtrl as ConfigCtrl,
-//   CloudWatchAnnotationsQueryCtrl as AnnotationsQueryCtrl,
-// };
