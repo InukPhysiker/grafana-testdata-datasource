b2227d0dfba8749ae283dd0f035247cc78d99ea7
InukPhysiker <InukPhysiker@gmail.com>
2020-02-29 09:26:57 -0600

    Formatting

--- src/app/plugins/datasource/cloudwatch-based/func_editor.ts
+++ src/app/plugins/datasource/cloudwatch-based/func_editor.ts
@@ -14,7 +14,8 @@ export function graphiteFuncEditor($compile: any, templateSrv: any) {
       onMoveRight="ctrl.handleMoveRight"
     /><span>(</span>
   `;
-  const paramTemplate = '<input type="text" style="display:none"' + ' class="input-small tight-form-func-param"></input>';
+  const paramTemplate =
+    '<input type="text" style="display:none"' + ' class="input-small tight-form-func-param"></input>';
 
   return {
     restrict: 'A',
