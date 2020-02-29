fe33552205820bdad3cffb2cf8d1dc178208423e
InukPhysiker <InukPhysiker@gmail.com>
2020-02-29 09:40:02 -0600

    clean-up comments

--- src/app/plugins/datasource/cloudwatch-based/components/ConfigEditor.tsx
+++ src/app/plugins/datasource/cloudwatch-based/components/ConfigEditor.tsx
@@ -1,19 +1,3 @@
-// import React, { PureComponent } from 'react';
-// import { FormLabel, Select, Input, Button } from '@grafana/ui';
-// import {
-//   DataSourcePluginOptionsEditorProps,
-//   onUpdateDatasourceJsonDataOptionSelect,
-//   onUpdateDatasourceOption,
-//   onUpdateDatasourceResetOption,
-//   onUpdateDatasourceJsonDataOption,
-//   onUpdateDatasourceSecureJsonDataOption,
-// } from '@grafana/data';
-// import { SelectableValue } from '@grafana/data';
-// import { getDatasourceSrv } from 'app/features/plugins/datasource_srv';
-// import EpicsDatasource from '../datasource';
-// import { EpicsJsonData, EpicsSecureJsonData } from '../types';
-// import { CancelablePromise, makePromiseCancelable } from 'app/core/utils/CancelablePromise';
-
 import React from 'react';
 import { DataSourceHttpSettings } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
